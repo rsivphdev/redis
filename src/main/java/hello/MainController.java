7c0d44dcbc0bf39ee9f1fd4289ed531b707322d9
lchukhov <leonid.chukhovsky@gov.bc.ca>
2018-11-07 13:24:08 -0800

    log level?!!!

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -1,6 +1,8 @@
 package hello;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping(path="/all")
     public @ResponseBody String testSpring() {
+
+        LOGGER.info("Hello, Controller");
         // This returns a JSON or XML with the users
         return "Test";
+
     }
 }
