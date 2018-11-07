9ac42c526d0ed28799acc54085aa2d88888d0655
lchukhov <leonid.chukhovsky@gov.bc.ca>
2018-11-06 17:33:00 -0800

    testing servlet

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -57,7 +57,8 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) throws InterruptedException {
 
 
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
+		//ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
 		/*StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
 		CountDownLatch latch = ctx.getBean(CountDownLatch.class);
